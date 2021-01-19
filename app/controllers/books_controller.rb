17d448f5904deabeccd1a98edeab0eec9f5bedc7
yuya-yuya <yytyfp5115@icloud.com>
2021-01-19 08:46:59 +0000

    additional commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,9 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
     redirect_to books_path
   end
 
