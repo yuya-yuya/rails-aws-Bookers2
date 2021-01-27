96461eb21851d6d0813d85b6c06f02aa162fd9e5
yuya-yuya <yytyfp5115@icloud.com>
2021-01-27 14:12:26 +0000

    additional commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+     redirect_to user_path(@user.id) 
   end
   
   def create
