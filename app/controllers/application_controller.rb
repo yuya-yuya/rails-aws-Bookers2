e079b24a57939e681651c9c1a653b7068f4a55f8
yuya-yuya <yytyfp5115@icloud.com>
2021-01-30 11:50:13 +0000

    additional commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
+  
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
 
   protected
 
